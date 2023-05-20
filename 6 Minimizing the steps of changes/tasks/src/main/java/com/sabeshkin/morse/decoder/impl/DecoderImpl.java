18323433814004e87078832a3d3cf95e4dfa11d7
FedorSabeshkin <fsabeshkin@mail.ru>
2023-05-20 21:55:34 +0300

    #2 (14) Практика в минимизировании шагов изменений
    Реализован метод конвертации буквы из Морзе в Латиницу

--- 6 Minimizing the steps of changes/tasks/src/main/java/com/sabeshkin/morse/decoder/impl/DecoderImpl.java
+++ 6 Minimizing the steps of changes/tasks/src/main/java/com/sabeshkin/morse/decoder/impl/DecoderImpl.java
@@ -1,9 +1,16 @@
 package com.sabeshkin.morse.decoder.impl;
 
 import com.sabeshkin.morse.decoder.api.Decoder;
+import com.sabeshkin.morse.decoder.api.MorseCodeLatinLetterMap;
 
 public class DecoderImpl
     implements Decoder {
+
+  private final MorseCodeLatinLetterMap morseCodeLatinLetterMap;
+
+  public DecoderImpl() {
+    morseCodeLatinLetterMap = new MorseCodeLatinLetterMapImpl();
+  }
 
   @Override
   public String decodePhrase(String morseCode) {
@@ -12,7 +19,7 @@ public class DecoderImpl
 
   @Override
   public String decodeLetter(String morseCode) {
-    return "E";
+    return morseCodeLatinLetterMap.get(morseCode);
   }
 
   @Override
